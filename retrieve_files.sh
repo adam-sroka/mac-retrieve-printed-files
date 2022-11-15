3b2c438391d718bcba138b8a57632040daea7915
Adam Sroka <git@adam.sr>
2022-11-15 12:27:07 +0000

    fix: failsafe cd exit and dont inject filename
    Fix ShellCheck SC2164, SC2156, and SC2034.

--- retrieve_files.sh
+++ retrieve_files.sh
@@ -6,13 +6,13 @@
 printed_files_directory="/var/spool/cups"
 
 # copy the whole CUPS directory to current directory
-cp -r printed_files_directory ./
+cp -r "$printed_files_directory" ./
 
 # add read and write permission to copied directory
 chmod 755 cups
 
 # change directory to the copied CUPS directory
-cd cups
+cd cups || exit
 
 # rename all pdf files to pdfs
-find . -depth -name "d*" -exec sh -c 'f="{}"; mv -- "$f" "${f%}.pdf"' \;
+find . -depth -name "d*" -exec sh -c 'f="$1"; mv -- "$f" "${f%}.pdf"' shell {} \;
